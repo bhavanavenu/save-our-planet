3edb17aba344e7790f92367f60c8ae825364451e
aparna pattathil <aparnapattathil@hotmail.com>
2018-06-28 21:30:21 +0200

    deploy on heroku

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const path         = require('path');
 const session    = require("express-session");
 const MongoStore = require('connect-mongo')(session);
 const flash      = require("connect-flash");
-    
+
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/project-2', {useMongoClient: true})
+  .connect(process.env.MONGODB_URI, {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {

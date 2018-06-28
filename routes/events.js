5b9f6ab6b555b950e48a1e737af7456e9556c6b6
Bhavana <bhavanavenugopal2@gmail.com>
2018-06-28 15:32:07 +0200

    add edit and delete features

--- routes/events.js
+++ routes/events.js
@@ -63,22 +63,13 @@ eventRoutes.get("/", (req, res) => {
     .populate('_participants')
       .then(event => {
         const participants = event._participants;
-        res.render("events/details",{event, partcipants: participants})
+        const comments = event.comments;
+        res.render("events/details",{event, participants: participants, comments: comments})
       })
       .catch(error => {
         console.log(error)
       })
   });
-
-//edit event 
-  // eventRoutes.get('/:eventId/edit', (req, res, next) => {
-   // let eventId = req.params.eventId;
-  //   Event.findById( req.params.eventId )
-  //     .then( event => {
-  //       res.render( 'events/event-edit', {event});
-  //     })
-  //     .catch( err => { throw err } );
-  // });
 
 eventRoutes.get('/:eventId/join',(req,res,next)=>{
   const joiningUserId = req.user._id;
@@ -92,12 +83,100 @@ eventRoutes.get('/:eventId/join',(req,res,next)=>{
       } else {
         res.redirect(`/events/${updatedEvent._id}`)
       }
-    } )
+    })
   } )
    //{ $push: {_participants: req.user._id} }
 
 })
 
+//adding comments on the event page
+
+eventRoutes.post('/:eventId/comment', (req, res, next) => {
+  let eventId = req.params.eventId;
+  let text = req.body.text;
+
+  Event.findById(eventId)
+  .then(event =>{
+    console.log("old   ",event);
+    
+    event.comments.unshift({
+      text,
+      _author: req.user._id,
+      createdAt: new Date()
+
+    });
+    event.save()
+    .then(updatedEvent=>{
+      console.log("newwwww ",updatedEvent)
+      res.redirect(`/events/${updatedEvent._id}`)
+      // res.render('details', updatedEvent)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+    
+    })
+  
+  });
+
+
+  //GET event-edit page
+  // eventRoutes.get('/event-edit/:eventId', (req, res, next) => {
+  //  let eventId = req.params.eventId;
+  //  console.log('eventId',eventId)
+  //   Event.findById(eventId)
+  //     .then(event => {
+  //       res.render('events/event-edit', {event});
+  //     })
+  //     .catch( err => { throw err } );
+  // });
+
+//edit event page
+  eventRoutes.get('/:eventId/edit', (req, res, next) => {
+    Event.findById( req.params.eventId )
+      .then( event=> {
+        console.log("hello")
+        res.render( 'events/event-edit', event );
+      })
+      .catch( err => { throw err } );
+  });
+
+
+  //update event page
+eventRoutes.post('/:eventId/update', (req, res, next) => {
+  let { 
+    name, 
+    location,
+    description,
+    date,
+    _participants,
+    comments } = req.body;
+
+    let updatedChanges = {name, 
+      location,
+      description,
+      date,
+      _participants,
+      comments
+    }
+  Event.findByIdAndUpdate( req.params.eventId, updatedChanges)
+    .then( event => {
+      console.log("hey")
+      res.redirect( `/events/${event._id}`);
+    })
+    .catch( err => { throw err } );
+});
+
+
+//delete event
+eventRoutes.get('/:eventId/delete', (req, res, next) => {
+  Event.findByIdAndRemove( req.params.eventId )
+    .then( () => {
+      console.log("event deleted");
+      res.redirect('/events');
+    })
+    .catch( err => { throw err } );
+});
 
  module.exports = eventRoutes ;
 
